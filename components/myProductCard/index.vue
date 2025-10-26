17bb4fcbc8b4b87dc9270987d0c5bf9b2af9bc71
meng1290 <843730982@qq.com>
2025-10-26 15:52:57 +0800

    创建商品列表组件

---
+++ components/myProductCard/index.vue
@@ -0,0 +1,94 @@
+<template>
+	<view class="myProductCard">
+		<view class="li" v-for="(item,i) in dataList" :key="i" @click="handleDetail(item)">
+			<view class="img-box">
+				<up-image class="image" width="100%" height="100%" :src="item.image" mode="aspectFill">
+		      <template #loading><up-loading-icon></up-loading-icon></template>
+					<template #error>
+						<view style="font-size: 28rpx;">加载失败</view>
+					</template>
+		    </up-image>
+			</view>
+			<view class="btns">
+				<view class="collect">
+					<view>
+						<up-icon :name="item.collectIcon?'star-fill':'star'" :color="item.collectIcon?'rgb(255, 149, 0)':'#000'"></up-icon>
+					</view>
+					<view class="collectNum">12344</view>
+				</view>
+				<view class="btn">
+					<up-button type="primary" color="#0166FE" shape="circle">画同款</up-button>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	import { reactive, ref, toRefs, unref, inject} from 'vue'
+	const props = defineProps({
+	  dataList: {
+	    type: Array,
+	    default: []
+	  }
+	})
+	
+	const handleDetail = (item) =>{
+		uni.navigateTo({
+			url:"/pages/index/product"
+		})
+	}
+</script>
+
+<style lang="scss" scoped>
+	.myProductCard{
+		width: 100%;
+		display: flex;
+		flex-wrap: wrap;
+		.li{
+			width: calc((100% - 20rpx) / 2);
+			margin-bottom: 30rpx;
+			&:nth-child(odd) {
+				margin-right: 20rpx;
+			}
+			.img-box{
+				width: 100%;
+				height: 580rpx;
+				background-color: rgb(243, 244, 246);
+				border-radius: 16rpx;
+				overflow: hidden;
+				.image{
+					width: 100%;
+					height: 100%;
+				}
+			}
+			.btns{
+				width: 100%;
+				height: 72rpx;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				margin-top: 20rpx;
+				.collect{
+					flex: 1;
+					display: flex;
+					align-items: center;
+					width: 0;
+					::v-deep .u-icon__icon{
+						font-size: 48rpx !important;
+					}
+					.collectNum{
+						font-size: 32rpx;
+						margin-left: 10rpx;
+						white-space:nowrap;
+						text-overflow:ellipsis; overflow:hidden;
+					}
+				}
+				.btn{
+					flex: 1;
+					padding: 0;
+				}
+			}
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -15,29 +15,7 @@
 			<up-button type="primary" color="#FF9500">开始学习</up-button>
 		</view>
 		<view class="list">
-			<view class="ul">
-				<view class="li" v-for="(item,i) in dataList" :key="i" @click="handleDetail(item)">
-					<view class="img-box">
-						<up-image class="image" width="100%" height="100%" :src="item.image" mode="aspectFill">
-              <template #loading><up-loading-icon></up-loading-icon></template>
-							<template #error>
-								<view style="font-size: 28rpx;">加载失败</view>
-							</template>
-            </up-image>
-					</view>
-					<view class="btns">
-						<view class="collect">
-							<view>
-								<up-icon :name="item.collectIcon?'star-fill':'star'" :color="item.collectIcon?'rgb(255, 149, 0)':'#000'"></up-icon>
-							</view>
-							<view class="collectNum">{{item.collectNum}}</view>
-						</view>
-						<view class="btn">
-							<up-button type="primary" color="#0166FE" shape="circle">画同款</up-button>
-						</view>
-					</view>
-				</view>
-			</view>
+			<myProductCard :dataList="dataList"></myProductCard>
 			<!-- 暂无数据 -->
 			<up-empty v-if="!dataList.length" text=" " mode="data" :icon="'/static/image/data.png'" />
 			<!-- 分页加载提示 -->
@@ -51,6 +29,7 @@
 	import { reactive, ref, toRefs, unref, inject} from 'vue'
 	import { onLoad, onPullDownRefresh, onReachBottom } from '@dcloudio/uni-app'
 	import { getModelList, getCategory } from '@/api/index.js'
+	import myProductCard from "@/components/myProductCard/index.vue"
 
 	import banner from '@/static/image/banner.png';
 	const statusBarHeight = uni.getSystemInfoSync().statusBarHeight
@@ -129,12 +108,6 @@
       loadStatus.value = 'loading'
       getList()
     }
-    
-		const handleDetail = (item) =>{
-			uni.navigateTo({
-				url:"/pages/index/product"
-			})
-		}
 </script>
 
 <style lang="scss" scoped>
@@ -172,57 +145,6 @@
 			padding: 0 12rpx;
 			box-sizing: border-box;
 			padding-bottom: 20px;
-			.ul{
-				width: 100%;
-				display: flex;
-				flex-wrap: wrap;
-				// gap: 30rpx 20rpx;
-				.li{
-					width: calc((100% - 20rpx) / 2);
-					margin-bottom: 30rpx;
-					&:nth-child(odd) {
-						margin-right: 20rpx;
-					}
-					.img-box{
-						width: 100%;
-						height: 580rpx;
-						background-color: rgb(243, 244, 246);
-						border-radius: 16rpx;
-						overflow: hidden;
-						.image{
-							width: 100%;
-							height: 100%;
-						}
-					}
-					.btns{
-						width: 100%;
-						height: 72rpx;
-						display: flex;
-						justify-content: space-between;
-						align-items: center;
-						margin-top: 20rpx;
-						.collect{
-							flex: 1;
-							display: flex;
-							align-items: center;
-							width: 0;
-							::v-deep .u-icon__icon{
-								font-size: 48rpx !important;
-							}
-							.collectNum{
-								font-size: 32rpx;
-								margin-left: 10rpx;
-								white-space:nowrap;
-								text-overflow:ellipsis; overflow:hidden;
-							}
-						}
-						.btn{
-							flex: 1;
-							padding: 0;
-						}
-					}
-				}
-			}
 		}
 	}
 	
